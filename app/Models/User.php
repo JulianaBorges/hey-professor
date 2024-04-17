16e3621066b53bfe796d26ccdd6a786d906f0f79
Juliana Borges <Juuh.borges@hotmail.com>
2024-04-17 18:28:37 -0300

    hey-10: refactoring the like vote function

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,13 +50,24 @@ class User extends Authenticatable
         return $this->hasMany(Vote::class);
     }
 
+//    public function like(question $question): void
+//    {
+//        Vote::query()->updateOrCreate([
+//            'question_id' => $question->id,
+//            "user_id"     => $this->id,
+//            'like'        => 1,
+//            'unlike'      => 0,
+//        ]);
+//    }
+
     public function like(question $question): void
     {
         Vote::query()->updateOrCreate(
             ['question_id' => $question->id],
             [
-                'like'   => 1,
-                'unlike' => 0,
+                'user_id' => $this->id,
+                'like'    => 1,
+                'unlike'  => 0,
             ]
         );
     }
